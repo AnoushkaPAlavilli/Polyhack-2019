b1400147e1ba5845f029bbc59661a7238cc3efeb
Ben Pradko <Benjamin.Pradko@tufts.edu>
2019-10-12 13:28:22 -0400

    here ya go

--- main.py
+++ main.py
@@ -6,13 +6,15 @@ Created on Fri Oct 11 19:41:41 2019
 @author: benpradko, anoushkaalavilli, ryangayhour
 """
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, World!"
-    
+    return render_template("format.html")
+
+@app.route("/about")
+
 if __name__ == "__main__":
     app.run(debug=True)
